8ff26e4e12da9d200f39c106c811842826e8fb42
Shwetang Mishra <shwetangmishra@Shwetangs-MacBook-Pro.local>
2021-07-21 13:52:52 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+
+setup(
+    name='src',version="0.0.1",
+    description = "wine q package",
+    author = "shwetang",
+    packages = find_packages(),
+    licence = 'MIT'
+)
+
